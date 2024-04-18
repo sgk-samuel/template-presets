c356f7366d918314d3896bd71a0ab4b3e72511f4
sgk <3228891558@qq.com>
2024-04-18 17:08:10 +0800

    main: initial project

---
+++ presets/project/vue/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+<template>
+
+
+
+
+</template>
+
+<style>
+
+
+
+</style>
---
+++ template/webpack.config.ts
@@ -0,0 +1,32 @@
+import { Configuration } from 'webpack'
+
+export default {
+  entry:'',
+  
+  output:{
+    path:'',
+    filename:''
+  },
+  
+  module:{
+    rules:[
+      {
+        
+      }
+    ]
+  },
+  
+  resolve:{
+    extensions:[],
+    alias:{
+      
+    }
+  },
+
+  plugins:[
+
+  ],
+
+  mode:'development'
+
+} satisfies Configuration
